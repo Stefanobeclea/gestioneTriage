b127766f40e42bd179ab4390f36c5f52ca8f8826
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-23 09:59:28 +0100

    Creato progetto

---
+++ src/main/java/it/prova/gestionepazienti/GestionepazientiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionepazienti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionepazientiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionepazientiApplication.class, args);
+	}
+
+}
