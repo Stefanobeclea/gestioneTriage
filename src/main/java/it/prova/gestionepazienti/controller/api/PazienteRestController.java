200c8148986484539a162047d6b52be2050f5cf8
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-24 11:33:01 +0100

    implementato get id Paziente

--- src/main/java/it/prova/gestionepazienti/controller/api/PazienteRestController.java
+++ src/main/java/it/prova/gestionepazienti/controller/api/PazienteRestController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionepazienti.dto.PazienteDTO;
+import it.prova.gestionepazienti.model.Paziente;
 import it.prova.gestionepazienti.service.PazienteService;
 
 @RestController
@@ -20,5 +22,10 @@ public class PazienteRestController {
 	@GetMapping
 	public List<PazienteDTO> getAll() {
 		return PazienteDTO.createPazienteDTOListFromModelList(pazienteService.listAll());
+	}
+	
+	@GetMapping("/{idInput}")
+	public Paziente getPaziente(@PathVariable(required = true) Long idInput) {
+		return pazienteService.get(idInput);
 	}
 }
