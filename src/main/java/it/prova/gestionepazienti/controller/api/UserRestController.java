d899a3af896d4a1083ff6793044b34c75784ad56
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-24 12:29:42 +0100

    implementato get Id User

--- src/main/java/it/prova/gestionepazienti/controller/api/UserRestController.java
+++ src/main/java/it/prova/gestionepazienti/controller/api/UserRestController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionepazienti.dto.UserDTO;
+import it.prova.gestionepazienti.exceptions.UserNotFoundException;
+import it.prova.gestionepazienti.model.User;
 import it.prova.gestionepazienti.service.UserService;
 
 @RestController
@@ -20,5 +23,15 @@ public class UserRestController {
 	@GetMapping
 	public List<UserDTO> getAll() {
 		return UserDTO.createUserDTOListFromModelList(userService.listAll());
+	}
+	
+	@GetMapping("/{id}")
+	public UserDTO findById(@PathVariable(value = "id", required = true) long id) {
+		User user = userService.get(id);
+
+		if (user == null)
+			throw new UserNotFoundException("User not found con id: " + id);
+
+		return UserDTO.buildUserDTOFromModel(user);
 	}
 }
